3996e795a4416d86e717bda389a673ed6bf91caf
Alyssa Liguori <alyssa.liguori@gmail.com>
2019-11-25 14:47:30 -0500

    Rearrange notebooks in logical order.

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -15,16 +15,17 @@ def create_perf_metrics(y_test, y_pred_test):
         conf_matrix(Dataframe): Comparison of actual results versus predicted. 
     '''
     cm = confusion_matrix(y_test, y_pred_test)
-    conf_matrix = pd.DataFrame(cm, index=['Nonvoter/Other','Clinton','Trump'], 
-                           columns=['Pred Nonvoter/Other','Pred Clinton','Pred Trump'])
+    conf_matrix = pd.DataFrame(cm, index=['Clinton','Trump', 'Nonvoter/Other'], 
+                           columns=['Pred Clinton','Pred Trump', 'Pred Nonvoter/Other',])
+    print(cm)
     print(conf_matrix, '\n')
     
-    pred_correct_clinton = cm[1][1]
-    total_clinton = sum(cm[1])
+    pred_correct_clinton = cm[0][0]
+    total_clinton = sum(cm[0])
     perc_correct_clinton = round(pred_correct_clinton/total_clinton, 3)*100
     
-    pred_correct_trump = cm[2][2]
-    total_trump = sum(cm[2])
+    pred_correct_trump = cm[1][1]
+    total_trump = sum(cm[1])
     perc_correct_trump = round(pred_correct_trump/total_trump, 3)*100
     
     print('\n', perc_correct_clinton, 'percent that were predicted Clinton were actually Clinton''\n',
