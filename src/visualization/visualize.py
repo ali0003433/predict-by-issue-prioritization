fd7c1046f3ba6438e90170d22dc7c6d0e7a712e4
Alyssa Liguori <alyssa.liguori@gmail.com>
2019-11-28 20:16:14 -0500

    Clean up notebook 2

---
+++ src/visualization/visualize.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_bar(x, y, palette, title, xlabels): 
+    plt.figure(figsize=(8, 7))
+    bar_plot = sns.barplot(x=x, y=y,palette=palette)
+    bar_plot.set_title(title)
+    bar_plot.set_xticklabels(rotation=90, labels=xlabels)
+    bar_plot.set_ylabel('Weighted frequency')
+    return bar_plot
+
+
+def plot_stacked(data, title):
+    palette_short = ['cornflowerblue','tomato','lightgrey']
+    stacked_plot = data.T.plot(kind='bar', stacked=True, color=palette_short)
+    stacked_plot.set_title(title)
+    pres_cat_list = ['Clinton', 'Trump', 'Other behavior']
+    stacked_plot.legend(pres_cat_list)
+    N = 5 
+    ind = np.arange(N)
+    stacked_plot.set_xlabel('')
+    x_ticks = ('Very','Somewhat','Not very', 'Unimportant', 'No Response')
+    y_label = 'Weighted frequency'
+    plt.xticks(ind, x_ticks)
+    stacked_plot.set_ylabel(y_label)
+    return stacked_plot
+
+def count_res(df):
+    count_dict = {1: 0, 2: 0, 3: 0, 4: 0, 8: 0}
+    for idx, row in df.iterrows():
+        for col in df.columns:
+            res = row[col]
+            count_dict[res] +=1
+    return count_dict
